f42b9205548e22ebf4b1751006ceb606a646cc4e
NinPhi <melmarka4@gmail.com>
2022-11-08 22:04:26 +0600

    bound images to props and commands

--- MauiGallery/MauiProgram.cs
+++ MauiGallery/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiGallery;
+﻿using MauiGallery.ViewModel;
+
+namespace MauiGallery;
 
 public static class MauiProgram
 {
@@ -12,6 +14,10 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services
+			.AddSingleton<MainPage>()
+			.AddSingleton<SharedViewModel>();
 
 		return builder.Build();
 	}
---
+++ MauiGallery/ViewModel/SharedViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MauiGallery.ViewModel;
+
+public partial class SharedViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<FileResult> images;
+
+    [ObservableProperty]
+    private FileResult target;
+
+    public SharedViewModel()
+    {
+        var dir = FileSystem.AppDataDirectory;
+        Images = new ObservableCollection<FileResult>(
+            Directory.GetFiles(dir).Select(path => new FileResult(path)));
+    }
+
+    [RelayCommand]
+    private void SetTarget(FileResult image)
+    {
+        if (image != null)
+            Target = image;
+    }
+
+    [RelayCommand]
+    private void Show()
+    {
+
+    }
+
+    [RelayCommand]
+    private async Task Add()
+    {
+
+    }
+
+    [RelayCommand]
+    private void Delete()
+    {
+        if (Images.Contains(Target))
+            Images.Remove(Target);
+    }
+}
